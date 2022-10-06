796d1da6c58319430fef7df08caed4fd4cf9fffb
Jhoan Ramirez <jramirezvyv@gmail.com>
2022-10-06 16:47:47 -0300

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,5 @@
+const shareBtn = document.querySelector('.btn');
+const socialIcon = document.querySelector('.share__media');
+
+
+shareBtn.addEventListener('click',()=>{socialIcon.classList.toggle('show')});
